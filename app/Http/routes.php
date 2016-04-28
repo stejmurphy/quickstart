544eba7ea13dad9bddb1be4e46539323ecdc05c8
stejmurphy <stephen.jd.murphy@gmail.com>
2016-04-28 15:56:54 +0100

    add delete functionality and CDNs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,7 +20,11 @@ use Illuminate\Http\Request;
  */
 
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
 
 
@@ -50,5 +54,7 @@ Route::post('/task', function(Request $request){
  *  Delete a task
  */
 Route::delete('/task/{task}', function(Task $task){
-    //TODO
+    $task->delete();
+
+    return redirect('/');
 });
