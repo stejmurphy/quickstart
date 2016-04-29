2733b576b3aad187bcaccce99e4b9a7b4f6d55df
stejmurphy <stephen.jd.murphy@gmail.com>
2016-04-29 14:05:39 +0100

    updated error

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,4 +17,30 @@ class TaskController extends Controller
 	{
 		$this->middleware('auth');
 	}
+
+	/**
+	 * Display a list of all of the user's task.
+	 *
+	 * @param  Request  $request
+	 * @return Response
+	 */
+	public function index(Request $request)
+	{
+		return view('tasks.index');
+	}
+
+	/**
+	 * Create a new task.
+	 *
+	 * @param  Request  $request
+	 * @return Response
+	 */
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'name' => 'required|max:255',
+		]);
+
+		// Create The Task...
+	}
 }
